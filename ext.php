b6ba0c109de7185b5c756aabb197c01679f21b35
ladyvipaka <ladyvipaka@yahoo.com>
2015-02-10 05:58:27 -0700

    first commit.
    Porting in necessary files.

---
+++ ext.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @package phpBB Extension - Preferences Expansion
+* @copyright (c) 2014 Esmaerin/Amelia Winstead
+* @license COPYRIGH
+*
+*/
+
+namespace vipaka\index_tabbed;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
